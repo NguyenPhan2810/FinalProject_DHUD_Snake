7d50ba53db4cccfb355e39c2de177588e2fd3fb0
Phan Quý Nguyên <pqn2810@gmail.com>
2021-05-14 01:53:01 +0700

    [ADD] Basic control

--- FinalProject_Snake/index.js
+++ FinalProject_Snake/index.js
@@ -1,31 +1,83 @@
+var LA = require("linear-algebra")();
+
 const canvas = document.querySelector("canvas");
-const cxt = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
+
+
+let pause = false;
+let player;
+
+class GameObject
+{
+    constructor(posX, posY)
+    {
+        GameObject.AllGameObjects = [];
+        GameObject.AllGameObjects.push(this);
+
+        this.posX = posX;
+        this.posY = posY;
+
+        this.forward = LA.Vector;
+    }
+
+    Update()
+    {
+        
+    }
+
+    Draw()
+    {
+        ctx.arc(this.posX, this.posY, 30, 0, Math.PI * 2, false);
+        ctx.lineWidth = 10;
+        // line color
+        ctx.strokeStyle = 'blue';
+        ctx.stroke();
+    }
+}
 
 main();
 function main()
 {
-    fitCanvas();
+    canvas.height = canvas.width = 500;
+    var parentStyle = canvas.parentElement.style;
+    parentStyle.textAlign = "center";
+    parentStyle.width = "100%";
 
     // Construction
-    var Player = new GameObject(30, 30);
+    player = new GameObject(30, 30);
 
-    // Main loop
-    while (true)
+    run();
+}
+
+function run()
+{
+    gameUpdate();
+    gameDraw();
+
+    requestAnimationFrame(run);
+}
+
+function gameUpdate()
+{
+    for (let i = 0; i < GameObject.AllGameObjects.length; ++i)
     {
-        gameUpdate();
+        let obj = GameObject.AllGameObjects[i];
+
+        obj.Update();
     }
 }
 
-function fitCanvas() 
+function gameDraw()
 {
-    canvas.height = canvas.width = innerHeight;
-}
+    ctx.beginPath();
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-function gameUpdate(){
-
-    for (obj in GameObject.AllGameObjects)
+    for (let i = 0; i < GameObject.AllGameObjects.length; ++i)
     {
-        obj.Update();
+        let obj = GameObject.AllGameObjects[i];
+
+        obj.Draw();
     }
 }
 
@@ -36,38 +88,20 @@ window.addEventListener("keydown", function (e)
 
     if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */)
     {
-        console.log("up");
+        player.posY -= 1.7;
     }
     else if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */)
     {
-        console.log("right");
+        player.posX += 1.7;
     }
     else if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */)
     {
-        console.log("down");
+        player.posY += 1.7;
     }
     else if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */)
     {
-        console.log("left");
+        player.posX -= 1.7;
     }
 
     e.preventDefault();
 }, true);
-
-
-class GameObject
-{
-    constructor(posX, posY)
-    {
-        this.posX = posX;
-        this.posy = posY;
-
-        GameObject.AllGameObjects = [];
-        AllGameObjects.push(this);
-    }
-
-    Update()
-    {
-        console.log(posX);
-    }
-}
